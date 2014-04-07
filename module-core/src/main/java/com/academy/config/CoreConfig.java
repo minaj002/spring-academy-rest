7a7d9bf8ad3b72fc0babcbe22ae65333ccb1c64d
minaj002 <minaj002@gmail.com>
2014-04-07 19:37:47 +0300

    add production database

--- module-core/src/main/java/com/academy/config/CoreConfig.java
+++ module-core/src/main/java/com/academy/config/CoreConfig.java
@@ -1,5 +1,7 @@
 package com.academy.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +12,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.mongodb.DB;
 import com.mongodb.MongoClient;
 
 @Configuration
@@ -20,7 +23,14 @@ public class CoreConfig {
 	
 	@Bean
 	public MongoDbFactory mongoDbFactory() throws Exception {
-		return new SimpleMongoDbFactory(new MongoClient(), "academy");
+		MongoClient mongoClient = new MongoClient("ds045137.mongolab.com",45137);
+		
+		DB db = mongoClient.getDB("heroku_app23331098");
+		
+		db.authenticate("heroku_app23331098", "r7sir4ocq66034jq4gs5svpvbu".toCharArray());
+		
+		return new SimpleMongoDbFactory(mongoClient, "heroku_app23331098");
+//		return new SimpleMongoDbFactory(new MongoClient(), "academy");
 	}
 	
 	@Bean
